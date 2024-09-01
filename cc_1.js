41a223d35ac815df65896c1d706838e14cc04035
Jorge Bernal <jorgebernal@usf.edu>
2024-09-01 10:55:28 -0400

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,10 @@
+// Task 1: Variables and Data Types
+
+let employeeName = "Jesus Vazquez";
+const employeeID = 117;
+var isActive = false;
+
+console.log(employeeName, typeof(employeeName));
+console.log(employeeID, typeof(employeeID));
+console.log(isActive, typeof(isActive));
+
