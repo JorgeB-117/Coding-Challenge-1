81623aa67c6094a345c324ee81d8951bd33e975e
Jorge Bernal <jorgebernal@usf.edu>
2024-09-01 12:05:15 -0400

    Task 3 - Financial Calculations

--- cc_1.js
+++ cc_1.js
@@ -18,3 +18,28 @@ const productDetails = {
 }
 
 console.log(products, productDetails);
+
+// Task 3: Assignment Operators
+
+let accountBalance = 2000;
+console.log("Initial Balance", accountBalance);
+
+// Deposit 500
+accountBalance += 500;
+console.log("After Deposit 500:", accountBalance);
+
+// Withdraw 250
+accountBalance -= 250;
+console.log("After Withdraw 250:", accountBalance);
+
+// Loan
+accountBalance *= 2;
+console.log("After Loan 2250:", accountBalance);
+
+// Purchase
+accountBalance /= 2;
+console.log("After purchase 2250:", accountBalance);
+
+// Transaction Fee
+accountBalance %= 1000;
+console.log("After transaction fee modulo 1000:", accountBalance);
