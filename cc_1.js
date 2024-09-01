0cacaf957c00112f4ba57fdf7e3ff1b467af9a2a
Jorge Bernal <jorgebernal@usf.edu>
2024-09-01 11:21:44 -0400

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -8,3 +8,13 @@ console.log(employeeName, typeof(employeeName));
 console.log(employeeID, typeof(employeeID));
 console.log(isActive, typeof(isActive));
 
+// Task 2: Compound Data Types
+
+let products = ["Monitor, Mouse, Keyboard"];
+const productDetails = {
+    inStock: 3,
+    name: products,
+    price: 957.00,
+}
+
+console.log(products, productDetails);
